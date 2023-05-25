b4aeb13f349a4100c9a89edc1aaf9a66f7d4281f
Shibbaz <kmosc@protonmail.com>
2023-05-25 17:00:18 +0200

    style: fixing db migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 202305201003000) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_25_162100) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 202305201003000) do
     t.integer "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.uuid "followers", default: [], null: false, array: true
   end
 
 end
