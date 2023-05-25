7b96cedb76991d7050097b9f81dd0edbfbf35e95
Shibbaz <kmosc@protonmail.com>
2023-05-25 11:04:29 +0200

    feat: conversion likes to [UserType] and adding batchloader

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -4,8 +4,23 @@ module Types
     field :insights, String, null: false
     field :question, String, null: false
     field :feeling, Int, null: false
-    field :likes, [ID], null: false
+    field :likes, [Types::UserType], null: false
+    field :likes_counter, Int, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def likes
+      BatchLoader::GraphQL.for(object.likes.uniq).batch(default_value: []) do |user_ids, loader|
+        User.where(id: user_ids).each { |user|
+          loader.call(object.likes.uniq) { |data|
+            data << user
+          }
+        }
+      end
+    end
+
+    def likes_counter
+      likes.as_json["batch_loader"].size
+    end
   end
 end
