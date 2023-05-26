dce77a40170ab4447dc98233bc6e0ccd72a12512
Shibbaz <kmosc@protonmail.com>
2023-05-26 21:54:00 +0200

    feat: checking if 'text' key exists?

--- app/contexts/helpers/versioning.rb
+++ app/contexts/helpers/versioning.rb
@@ -5,7 +5,7 @@ module Contexts
         raise Contexts::Helpers::Errors::VersionsNotFoundError if log.nil?
 
         log.data["h"][1...].pluck("c").reverse.select { |data|
-          data.key?("insights") == true || data.key?("question") == true
+          data.key?("insights") == true || data.key?("question") == true || data.key?("text") == true
         }.each_with_index.map { |item, index|
           {
             id: index
