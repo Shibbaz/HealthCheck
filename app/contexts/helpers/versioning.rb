33dff3c456e1ccec256cf1a438b5300420ef1eb2
Shibbaz <kmosc@protonmail.com>
2023-05-26 20:07:43 +0200

    style: delete byebug extra line

--- app/contexts/helpers/versioning.rb
+++ app/contexts/helpers/versioning.rb
@@ -2,7 +2,6 @@ module Contexts
   module Helpers
     class Versioning
       def self.versions(log)
-        byebug
         log.data["h"][1...].pluck("c").reverse.select { |data|
           data.key?("insights") == true || data.key?("question") == true
         }.each_with_index.map { |item, index|
