a46c9ef31151469d506aa249e15dc94facbd83ff
Shibbaz <kmosc@protonmail.com>
2023-05-26 12:38:57 +0200

    db: Migrations

---
+++ db/migrate/20230526121600_add_user_id_to_comments.rb
@@ -0,0 +1,6 @@
+class AddUserIdToComments < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :comments, :user, foreign_key: true, type: :uuid
+
+  end
+end
---
+++ db/migrate/20230526300000_add_post_id_to_comments.rb
@@ -0,0 +1,6 @@
+class AddPostIdToComments < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :comments, :post, foreign_key: true, type: :uuid
+
+  end
+end
