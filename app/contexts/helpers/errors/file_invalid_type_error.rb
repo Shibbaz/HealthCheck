6e656b16c568e03c35a9847c3de6e62ef962ec17
Shibbaz <kmosc@protonmail.com>
2023-05-27 13:19:54 +0200

    feat: file upload

---
+++ app/contexts/helpers/errors/file_invalid_type_error.rb
@@ -0,0 +1,11 @@
+module Contexts
+  module Helpers
+    module Errors
+      class FileInvalidTypeError < ActiveRecord::RecordNotFound
+        def message
+          "File is not found"
+        end
+      end
+    end
+  end
+end
