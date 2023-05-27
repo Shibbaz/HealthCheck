2ad4dafcc05e025e3ea73ea7f6361888741c8f0d
Shibbaz <kmosc@protonmail.com>
2023-05-27 02:58:29 +0200

    feat: adding methods to load single and array of objects - batchloader

---
+++ app/contexts/helpers/records.rb
@@ -0,0 +1,22 @@
+module Contexts
+    module Helpers
+        class Records
+            def self.load(adapter:, id:)
+                data = BatchLoader.for(id).batch do |ids, loader|
+                    adapter.where(id: ids).each { |object| loader.call(object.id, object) }
+                end
+                return data
+            end
+
+            def self.load_array(adapter:, array:)
+                BatchLoader::GraphQL.for(array).batch(default_value: []) do |ids, loader|
+                    User.where(id: ids).each { |container|
+                      loader.call(array) { |data|
+                        data << container
+                      }
+                    }
+                end
+            end
+        end
+    end
+end
