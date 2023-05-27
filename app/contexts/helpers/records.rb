30b14e83f9daceda1b47172eb32dc9c1f5d7967a
Shibbaz <kmosc@protonmail.com>
2023-05-27 06:15:26 +0200

    fix: methods works correctly

--- app/contexts/helpers/records.rb
+++ app/contexts/helpers/records.rb
@@ -1,22 +1,21 @@
 module Contexts
-    module Helpers
-        class Records
-            def self.load(adapter:, id:)
-                data = BatchLoader.for(id).batch do |ids, loader|
-                    adapter.where(id: ids).each { |object| loader.call(object.id, object) }
-                end
-                return data
-            end
-
-            def self.load_array(adapter:, array:)
-                BatchLoader::GraphQL.for(array).batch(default_value: []) do |ids, loader|
-                    adapter.where(id: ids).each { |container|
-                      loader.call(array) { |data|
-                        data << container
-                      }
-                    }
-                end
-            end
+  module Helpers
+    class Records
+      def self.load(adapter:, id:)
+        data = BatchLoader.for(id).batch do |ids, loader|
+          adapter.where(id: ids).each { |object| loader.call(object.id, object) }
         end
+        return data
+      end
+
+      def self.load_array(adapter:, array:)
+        data = BatchLoader.for(array).batch(default_value: []) do |object_ids, loader|
+          adapter.where(id: object_ids).each { |object|
+            loader.call(array) { |memo| memo << object }
+          }
+        end
+        return data
+      end
     end
+  end
 end
