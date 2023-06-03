c6d05572c86c71a4a7091d8dbbaaf072ed0a03ef
Shibbaz <107750344+Shibbaz@users.noreply.github.com>
2023-06-03 20:10:11 +0200

    [HC-033] Update User Feature (#36)

---
+++ app/concepts/users/commands/update_user.rb
@@ -0,0 +1,21 @@
+module Concepts
+    module Users
+      module Commands
+        class UpdateUser
+          def call(event)
+            stream = event.data
+            error_type = Services::Records.build_error(adapter: stream[:adapter])
+            current_user = stream[:current_user]
+            current_user.nil? ? (raise error_type) : nil
+            !current_user.email.eql?(stream[:args][:email]) ? (raise Concepts::Users::Errors::UserEmailWasIncorrectError.new) : nil
+            
+            current_user.with_lock do
+              current_user.update!(stream[:args])
+            end
+            UpdateUserJob.with.perform(stream)
+          end
+        end
+      end
+    end
+  end
+  
---
+++ app/concepts/users/errors/user_email_was_incorrect_error.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Concepts
+  module Users
+    module Errors
+      class UserEmailWasIncorrectError < ActiveRecord::RecordNotFound
+        def message
+          'User email was incorrect!'
+        end
+      end
+    end
+  end
+end
