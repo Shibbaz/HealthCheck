e6faab84c736656a43a0526be0fee7c2a4fc00cf
Shibbaz <kmosc@protonmail.com>
2023-06-06 16:19:38 +0200

    refactor: rearrange code

--- app/lib/services/suggestions.rb
+++ app/lib/services/suggestions.rb
@@ -1,29 +1,46 @@
 module Services
     class Suggestions
         def self.suggest(user:)
-            time = DateTime.now
-            user_id = user.id
-            command = Suggestion.where(
-                receiver_id: user_id,
-                created_at: (time.beginning_of_day..time.end_of_day)
-            ).load_async.order(created_at: :desc).limit(10)
-            connections = ->(followers) { 
-                ids = user.followers & followers
-                User.where(id: ids).load_async.map {|user|{
-                    id: user.id,
-                    name: user.name,
-                    followersCount: user.followers.count
-                }}
-            }
-            authors = User.where(id: command.pluck(:author_id)).load_async.uniq
-            authors = authors.map {|author| 
-                {
-                    id: author.id,
-                    name: author.name,
-                    followersCount: author.followers.count, 
-                    followers: connections.call(author.followers)
+            command = self.query(receiver_id: user.id)
+            authors = -> {
+                User.where(id: command.pluck(:author_id)).load_async.uniq.map {|entity|
+                    mutual = ->(followers) { 
+                        ids = user.followers & followers
+                        self.connectionsNearby(ids)
+                    }
+                    {
+                        id: entity.id,
+                        name: entity.name,
+                        followersCount: entity.followers.count, 
+                        mutual: mutual.call(user.followers & entity.followers)
+                    }
                 }
             }
+            payload(user: user, authors: authors.call)
+        end
+
+        def self.create(receiver_id:, author_id:)
+            Suggestion.create(receiver_id: receiver_id, author_id: author_id)
+        end
+
+        private
+
+        def self.query(receiver_id:, time: DateTime.now)
+            Suggestion.where(
+                receiver_id: receiver_id,
+                created_at: (time.beginning_of_day..time.end_of_day)
+            ).load_async.order(created_at: :desc).limit(10)
+        end
+
+        def self.connectionsNearby(ids)
+            User.where(id: ids).load_async.map {|user|{
+                id: user.id,
+                name: user.name,
+                followersCount: user.followers.count
+            }}
+        end
+
+        def self.payload(user:, authors:)
             { 
                 user: {
                     id: user.id,
@@ -32,10 +49,6 @@ module Services
                 }, 
                 authors: authors
             }
-        end
-
-        def self.create(receiver_id:, author_id:)
-            Suggestion.create(receiver_id: receiver_id, author_id: author_id)
         end
     end
 end
