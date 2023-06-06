b441e90b1ea4cd3c943f245e029159d9a5d1c06a
Shibbaz <kmosc@protonmail.com>
2023-06-06 18:06:54 +0200

    feat: sort_by occurances of most interested user

--- app/lib/services/suggestions.rb
+++ app/lib/services/suggestions.rb
@@ -3,17 +3,21 @@ module Services
         def self.suggest(user:)
             command = self.query(receiver_id: user.id)
             authors = -> {
-                User.where(id: command.pluck(:author_id)).load_async.uniq.map {|entity|
+                User.where(id: command.pluck(:author_id)).load_async.map {|entity|
                     mutual = ->(followers) { 
                         ids = user.followers & followers
                         self.connectionsNearby(ids)
                     }
                     self.userPayload(entity).merge({ 
-                        mutual: mutual.call(entity.followers)
-                    })
+                            mutual: mutual.call(entity.followers)
+                        }, occurances: command.reject{|object| object.author_id != entity.id}.count
+                    )
                 }
             }
-            payload(user: user, authors: authors.call)
+            payload(
+                user: user, 
+                authors: authors.call.sort_by { |hsh| hsh[:occurances] }.reverse
+            )
         end
 
         def self.create(receiver_id:, author_id:)
@@ -23,10 +27,10 @@ module Services
         private
 
         def self.query(receiver_id:, time: DateTime.now)
-            Suggestion.where(
+            sug= Suggestion.where(
                 receiver_id: receiver_id,
                 created_at: (time.beginning_of_day..time.end_of_day)
-            ).load_async.order(created_at: :desc).limit(10)
+            ).load_async.order(created_at: :desc).limit(20)
         end
 
         def self.connectionsNearby(ids)
