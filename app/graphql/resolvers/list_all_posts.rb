b5f87c9de73efe1f1dedcb5c492313ddff394834
Shibbaz <kmosc@protonmail.com>
2023-06-07 14:00:47 +0200

    feat: apply on graphql mutation and resolver

--- app/graphql/resolvers/list_all_posts.rb
+++ app/graphql/resolvers/list_all_posts.rb
@@ -9,12 +9,29 @@ module Resolvers
     argument :page, Integer, required: false
     argument :limit, Integer, required: false
     argument :filters, Types::PostFilter, required: false
+    argument :user_id, ID, required: false
+
 
     def resolve(**args)
       Services::Validations::Authenticate.call(context:)
-      args = args.merge({
-                          user_id: context[:current_user].id
-                        })
+      
+      if !args[:user_id].nil?
+        user ||= User.find(args[:user_id])
+        raise Concepts::Users::Errors::UserNotFoundError if user == nil
+        visibility = user.followers.include?(context[:current_user])
+        args = args.merge(
+          {
+            visibility: visibility,
+          }
+        )
+      end
+
+      if args[:user_id].nil?
+        args = args.merge({
+          user_id: context[:current_user].id,
+          visibility: nil
+        })
+      end
       Concepts::Posts::Queries::AllRecords.new.call(args:).page(args[:page]).per(args[:limit])
     rescue ActiveRecord::RecordNotFound => e
       raise GraphQL::ExecutionError, e.message
