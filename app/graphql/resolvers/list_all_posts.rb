2e4061671455659cb0a377e189792f555366c80e
Kamil Mościszko <107750344+Shibbaz@users.noreply.github.com>
2023-06-07 15:53:55 +0200

    [HC-041] Posts Visibility (#44)

--- app/graphql/resolvers/list_all_posts.rb
+++ app/graphql/resolvers/list_all_posts.rb
@@ -9,12 +9,31 @@ module Resolvers
     argument :page, Integer, required: false
     argument :limit, Integer, required: false
     argument :filters, Types::PostFilter, required: false
+    argument :usr, ID, required: false, default_value: nil
+
 
     def resolve(**args)
       Services::Validations::Authenticate.call(context:)
-      args = args.merge({
-                          user_id: context[:current_user].id
-                        })
+      
+      if args[:usr].present?
+        user ||= User.find(args[:usr])
+        raise Concepts::Users::Errors::UserNotFoundError if user == nil
+        visibility = user.followers.include?(context[:current_user].id)
+        args = args.merge(
+          {
+            visibility: visibility,
+          }
+        )
+        return Concepts::Posts::Queries::AllRecords.new.call(args:).page(args[:page]).per(args[:limit])
+        raise GraphQL::ExecutionError, e.message
+      end
+
+      if args[:usr].nil?
+        args = args.merge({
+          user_id: context[:current_user].id,
+          visibility: false
+        })
+      end
       Concepts::Posts::Queries::AllRecords.new.call(args:).page(args[:page]).per(args[:limit])
     rescue ActiveRecord::RecordNotFound => e
       raise GraphQL::ExecutionError, e.message
